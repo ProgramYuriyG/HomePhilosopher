34be4bff871fc27bd8fd7f91c3b8c8061ee89e00
Yuriy Glukhov <yuriy.glukhov@vk.ai>
2020-11-15 03:08:15 -0500

    altered keys so theyre valid

--- HomePhilosopher/__main__.py
+++ HomePhilosopher/__main__.py
@@ -1,8 +1,9 @@
 from HomePhilosopher.Api_Connector.api_communicator import *
 
 if __name__ == '__main__':
+    pass
     # create_county_dictionary()
     # get_crime_statistics('harrison', 'oh')
     # get_climate_information(19.716698, -155.08688)
     # get_environmental_data(19.716698, -155.08688)
-    create_environmental_json('39083')
+    # create_environmental_json('39083')
--- countymap/countymap/settings.py
+++ countymap/countymap/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-from HomePhilosopher.Api_Connector.API_KEYS import Django_SECRET_KEY
+import countymap.API_KEYS as sk
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Django_SECRET_KEY
+SECRET_KEY = sk.Django_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
